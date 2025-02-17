688453c49939f5fcac8fea2e3c4c04067044fb30
Giioya <gioda2405@gmail.com>
2025-02-16 22:49:50 -0500

    Actualiza Eruda a ClientErudaProvider y corrige errores en layout.tsx

--- components/Eruda/ClientErudaProvider.tsx
+++ components/Eruda/ClientErudaProvider.tsx
@@ -5,14 +5,16 @@ import eruda from "eruda";
 
 export const ClientErudaProvider = (props: { children: ReactNode }) => {
     useEffect(() => {
+        // Solo en el cliente y en desarrollo
         if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
-        try {
-            eruda.init();
-        } catch (error) {
-            console.log("Eruda failed to initialize", error);
-        }
+            try {
+                eruda.init();
+            } catch (error) {
+                console.log("Eruda failed to initialize", error);
+            }
         }
     }, []);
 
     return <>{props.children}</>;
 };
+
